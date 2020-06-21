cf8cf5d128ea37d5783a29f8cb68e43ef63fb378
Waleed Jubeh <wsameer70@gmail.com>
2020-06-21 20:10:27 +0300

    add !imortant word and make readme v1

--- app.js
+++ app.js
@@ -26,129 +26,128 @@ let bigScreenResponsive = {
 }
 
 let manyClassesOneAttribute = [
-    new CssClass("m", ["margin"], "%s:%dpx"),
-    new CssClass("wpx", ["width"], "%s:%dpx"),
-    new CssClass("hpx", ["height"], "%s:%dpx"),
-    new CssClass("m-r", ["margin-right"], "%s:%dpx;"),
-    new CssClass("m-l", ["margin-left"], "%s:%dpx;"),
-    new CssClass("m-t", ["margin-top"], "%s:%dpx;"),
-    new CssClass("m-b", ["margin-bottom"], "%s:%dpx;"),
-    new CssClass("p", ["padding"], "%s:%dpx"),
-    new CssClass("p-r", ["padding-right"], "%s:%dpx;"),
-    new CssClass("p-l", ["padding-left"], "%s:%dpx;"),
-    new CssClass("p-t", ["padding-top"], "%s:%dpx;"),
-    new CssClass("p-b", ["padding-bottom"], "%s:%dpx;"),
-    new CssClass("fz", ["font-size"], "%s:%dpx;"),
+    new CssClass("m", ["margin"], "%s:%dpx !important"),
+    new CssClass("wpx", ["width"], "%s:%dpx !important"),
+    new CssClass("hpx", ["height"], "%s:%dpx !important"),
+    new CssClass("m-r", ["margin-right"], "%s:%dpx !important"),
+    new CssClass("m-l", ["margin-left"], "%s:%dpx !important"),
+    new CssClass("m-t", ["margin-top"], "%s:%dpx !important"),
+    new CssClass("m-b", ["margin-bottom"], "%s:%dpx !important"),
+    new CssClass("p", ["padding"], "%s:%dpx !important"),
+    new CssClass("p-r", ["padding-right"], "%s:%dpx !important"),
+    new CssClass("p-l", ["padding-left"], "%s:%dpx !important"),
+    new CssClass("p-t", ["padding-top"], "%s:%dpx !important"),
+    new CssClass("p-b", ["padding-bottom"], "%s:%dpx !important"),
+    new CssClass("fz", ["font-size"], "%s:%dpx !important"),
 ]
 let manyClassesManyAttribute = [
-    new CssClass("m-x", ["margin-right", "margin-left"], "%s:%dpx;"),
-    new CssClass("m-y", ["margin-top", "margin-bottom"], "%s:%dpx;"),
-    new CssClass("p-x", ["margin-right", "margin-left"], "%s:%dpx;"),
-    new CssClass("p-y", ["padding-top", "padding-bottom"], "%s:%dpx;"),
+    new CssClass("m-x", ["margin-right", "margin-left"], "%s:%dpx !important;"),
+    new CssClass("m-y", ["margin-top", "margin-bottom"], "%s:%dpx !important;"),
+    new CssClass("p-x", ["margin-right", "margin-left"], "%s:%dpx !important;"),
+    new CssClass("p-y", ["padding-top", "padding-bottom"], "%s:%dpx !important;"),
 ]
 let manyClassesOneAttributeMeduimScreen = [
-    new CssClass("m-md", ["margin"], "%s:%dpx"),
-    new CssClass("m-r-md", ["margin-right"], "%s:%dpx;"),
-    new CssClass("m-l-md", ["margin-left"], "%s:%dpx;"),
-    new CssClass("m-t-md", ["margin-top"], "%s:%dpx;"),
-    new CssClass("m-b-md", ["margin-bottom"], "%s:%dpx;"),
-    new CssClass("p-md", ["padding"], "%s:%dpx"),
-    new CssClass("p-r-md", ["padding-right"], "%s:%dpx;"),
-    new CssClass("p-l-md", ["padding-left"], "%s:%dpx;"),
-    new CssClass("p-t-md", ["padding-top"], "%s:%dpx;"),
-    new CssClass("p-b-md", ["padding-bottom"], "%s:%dpx;"),
-    new CssClass("fz-md", ["font-size"], "%s:%dpx;"),
-    new CssClass("wpx-md", ["width"], "%s:%dpx"),
-    new CssClass("hpx-md", ["height"], "%s:%dpx"),
+    new CssClass("m-md", ["margin"], "%s:%dpx !important"),
+    new CssClass("m-r-md", ["margin-right"], "%s:%dpx !important"),
+    new CssClass("m-l-md", ["margin-left"], "%s:%dpx !important"),
+    new CssClass("m-t-md", ["margin-top"], "%s:%dpx !important"),
+    new CssClass("m-b-md", ["margin-bottom"], "%s:%dpx !important"),
+    new CssClass("p-md", ["padding"], "%s:%dpx !important"),
+    new CssClass("p-r-md", ["padding-right"], "%s:%dpx !important"),
+    new CssClass("p-l-md", ["padding-left"], "%s:%dpx !important"),
+    new CssClass("p-t-md", ["padding-top"], "%s:%dpx !important"),
+    new CssClass("p-b-md", ["padding-bottom"], "%s:%dpx !important"),
+    new CssClass("fz-md", ["font-size"], "%s:%dpx !important"),
+    new CssClass("wpx-md", ["width"], "%s:%dpx !important"),
+    new CssClass("hpx-md", ["height"], "%s:%dpx !important"),
 ]
 let manyClassesOneAttributeSmallScreen = [
-    new CssClass("m-sm", ["margin"], "%s:%dpx"),
-    new CssClass("m-r-sm", ["margin-right"], "%s:%dpx;"),
-    new CssClass("m-l-sm", ["margin-left"], "%s:%dpx;"),
-    new CssClass("m-t-sm", ["margin-top"], "%s:%dpx;"),
-    new CssClass("m-b-sm", ["margin-bottom"], "%s:%dpx;"),
-    new CssClass("p-sm", ["padding"], "%s:%dpx"),
-    new CssClass("p-r-sm", ["padding-right"], "%s:%dpx;"),
-    new CssClass("p-l-sm", ["padding-left"], "%s:%dpx;"),
-    new CssClass("p-t-sm", ["padding-top"], "%s:%dpx;"),
-    new CssClass("p-b-sm", ["padding-bottom"], "%s:%dpx;"),
-    new CssClass("fz-sm", ["font-size"], "%s:%dpx;"),
-    new CssClass("wpx-sm", ["width"], "%s:%dpx"),
-    new CssClass("hpx-sm", ["height"], "%s:%dpx"),
+    new CssClass("m-sm", ["margin"], "%s:%dpx !important"),
+    new CssClass("m-r-sm", ["margin-right"], "%s:%dpx !important"),
+    new CssClass("m-l-sm", ["margin-left"], "%s:%dpx !important"),
+    new CssClass("m-t-sm", ["margin-top"], "%s:%dpx !important"),
+    new CssClass("m-b-sm", ["margin-bottom"], "%s:%dpx !important"),
+    new CssClass("p-sm", ["padding"], "%s:%dpx !important"),
+    new CssClass("p-r-sm", ["padding-right"], "%s:%dpx !important"),
+    new CssClass("p-l-sm", ["padding-left"], "%s:%dpx !important"),
+    new CssClass("p-t-sm", ["padding-top"], "%s:%dpx !important"),
+    new CssClass("p-b-sm", ["padding-bottom"], "%s:%dpx !important"),
+    new CssClass("fz-sm", ["font-size"], "%s:%dpx !important"),
+    new CssClass("wpx-sm", ["width"], "%s:%dpx !important"),
+    new CssClass("hpx-sm", ["height"], "%s:%dpx !important"),
 
 ]
 let manyClassesOneAttributeBigScreen = [
-    new CssClass("m-lg", ["margin"], "%s:%dpx"),
-    new CssClass("m-r-lg", ["margin-right"], "%s:%dpx;"),
-    new CssClass("m-l-lg", ["margin-left"], "%s:%dpx;"),
-    new CssClass("m-t-lg", ["margin-top"], "%s:%dpx;"),
-    new CssClass("m-b-lg", ["margin-bottom"], "%s:%dpx;"),
-    new CssClass("p-lg", ["padding"], "%s:%dpx"),
-    new CssClass("p-r-lg", ["padding-right"], "%s:%dpx;"),
-    new CssClass("p-l-lg", ["padding-left"], "%s:%dpx;"),
-    new CssClass("p-t-lg", ["padding-top"], "%s:%dpx;"),
-    new CssClass("p-b-lg", ["padding-bottom"], "%s:%dpx;"),
-    new CssClass("fz-lg", ["font-size"], "%s:%dpx;"),
-    new CssClass("wpx-lg", ["width"], "%s:%dpx"),
-    new CssClass("hpx-lg", ["height"], "%s:%dpx"),
+    new CssClass("m-lg", ["margin"], "%s:%dpx !important"),
+    new CssClass("m-r-lg", ["margin-right"], "%s:%dpx !important"),
+    new CssClass("m-l-lg", ["margin-left"], "%s:%dpx !important"),
+    new CssClass("m-t-lg", ["margin-top"], "%s:%dpx !important"),
+    new CssClass("m-b-lg", ["margin-bottom"], "%s:%dpx !important"),
+    new CssClass("p-lg", ["padding"], "%s:%dpx !important"),
+    new CssClass("p-r-lg", ["padding-right"], "%s:%dpx !important"),
+    new CssClass("p-l-lg", ["padding-left"], "%s:%dpx !important"),
+    new CssClass("p-t-lg", ["padding-top"], "%s:%dpx !important"),
+    new CssClass("p-b-lg", ["padding-bottom"], "%s:%dpx !important"),
+    new CssClass("fz-lg", ["font-size"], "%s:%dpx !important"),
+    new CssClass("wpx-lg", ["width"], "%s:%dpx !important"),
+    new CssClass("hpx-lg", ["height"], "%s:%dpx !important"),
 
 ]
 let results = "";
 results = manyClassesOneAttribute.reduce(reducerOneAttribute, "");
 results += manyClassesManyAttribute.reduce(reduceManyAttribute, "");
 // Generate auto classes
-results += CssClass.directPrint("m-auto","margin",".%s{%s:auto}");
-results += CssClass.directPrint("p-auto","padding",".%s{%s:auto}");
-results += CssClass.directPrint("f-box","display",".%s{%s:flex}");
-results += CssClass.directPrint("f-t-c","align-items",".%s{%s:center}");
-results += CssClass.directPrint("f-t-start","align-items",".%s{%s:flex-start}");
-results += CssClass.directPrint("f-t-flex","align-items",".%s{%s:flex-end}");
-results += CssClass.directPrint("f-j-c","justify-content",".%s{%s:center}");
-results += CssClass.directPrint("f-j-c","justify-content",".%s{%s:center}");
-results += CssClass.directPrint("f-j-start","justify-content",".%s{%s:flex-start}");
-results += CssClass.directPrint("f-j-between","justify-content",".%s{%s:space-between}");
-results += CssClass.directPrint("f-j-around","justify-content",".%s{%s:space-around}");
-results += CssClass.directPrint("f-j-evenly","justify-content",".%s{%s:space-evenly}");
-results += CssClass.directPrint("f-gr-1","flex-grow",".%s{%s:1}");
-results += CssClass.directPrint("f-gr-0","flex-grow",".%s{%s:0}");
-results += CssClass.directPrint("f-sh-1","flex-grow",".%s{%s:1}");
-results += CssClass.directPrint("f-sh-0","flex-grow",".%s{%s:0}");
+results += CssClass.directPrint("m-auto", "margin", ".%s{%s:auto}");
+results += CssClass.directPrint("p-auto", "padding", ".%s{%s:auto}");
+results += CssClass.directPrint("f-box", "display", ".%s{%s:flex}");
+results += CssClass.directPrint("f-t-c", "align-items", ".%s{%s:center}");
+results += CssClass.directPrint("f-t-start", "align-items", ".%s{%s:flex-start}");
+results += CssClass.directPrint("f-t-flex", "align-items", ".%s{%s:flex-end}");
+results += CssClass.directPrint("f-j-c", "justify-content", ".%s{%s:center}");
+results += CssClass.directPrint("f-j-start", "justify-content", ".%s{%s:flex-start}");
+results += CssClass.directPrint("f-j-between", "justify-content", ".%s{%s:space-between}");
+results += CssClass.directPrint("f-j-around", "justify-content", ".%s{%s:space-around}");
+results += CssClass.directPrint("f-j-evenly", "justify-content", ".%s{%s:space-evenly}");
+results += CssClass.directPrint("f-gr-1", "flex-grow", ".%s{%s:1}");
+results += CssClass.directPrint("f-gr-0", "flex-grow", ".%s{%s:0}");
+results += CssClass.directPrint("f-sh-1", "flex-grow", ".%s{%s:1}");
+results += CssClass.directPrint("f-sh-0", "flex-grow", ".%s{%s:0}");
 
-    let responsive = "";
+let responsive = "";
 
 responsive = manyClassesOneAttributeSmallScreen.reduce(reducerOneAttribute, "");
-responsive += CssClass.directPrint("m-r-sm-auto","margin-right",".%s{%s:auto}");
-responsive += CssClass.directPrint("p-r-sm-auto","padding-right",".%s{%s:auto}");
-responsive += CssClass.directPrint("m-l-sm-auto","margin-left",".%s{%s:auto}");
-responsive += CssClass.directPrint("p-l-sm-auto","padding-left",".%s{%s:auto}");
-responsive += CssClass.directPrint("m-t-sm-auto","margin-top",".%s{%s:auto}");
-responsive += CssClass.directPrint("p-t-sm-auto","padding-top",".%s{%s:auto}");
-responsive += CssClass.directPrint("m-b-sm-auto","margin-bottom",".%s{%s:auto}");
-responsive += CssClass.directPrint("p-b-sm-auto","padding-bottom",".%s{%s:auto}");
+responsive += CssClass.directPrint("m-r-sm-auto", "margin-right", ".%s{%s:auto !important}");
+responsive += CssClass.directPrint("p-r-sm-auto", "padding-right", ".%s{%s:auto !important}");
+responsive += CssClass.directPrint("m-l-sm-auto", "margin-left", ".%s{%s:auto !important}");
+responsive += CssClass.directPrint("p-l-sm-auto", "padding-left", ".%s{%s:auto !important}");
+responsive += CssClass.directPrint("m-t-sm-auto", "margin-top", ".%s{%s:auto !important}");
+responsive += CssClass.directPrint("p-t-sm-auto", "padding-top", ".%s{%s:auto !important}");
+responsive += CssClass.directPrint("m-b-    sm-auto", "margin-bottom", ".%s{%s:auto !important}");
+responsive += CssClass.directPrint("p-b-sm-auto", "padding-bottom", ".%s{%s:auto !important}");
 
 results += responsiveRapper(smallScreenResponsive, responsive);
 
 responsive = manyClassesOneAttributeMeduimScreen.reduce(reducerOneAttribute, "");
-responsive += CssClass.directPrint("m-r-md-auto","margin-right",".%s{%s:auto}");
-responsive += CssClass.directPrint("p-r-md-auto","padding-right",".%s{%s:auto}");
-responsive += CssClass.directPrint("m-l-md-auto","margin-left",".%s{%s:auto}");
-responsive += CssClass.directPrint("p-l-md-auto","padding-left",".%s{%s:auto}");
-responsive += CssClass.directPrint("m-t-md-auto","margin-top",".%s{%s:auto}");
-responsive += CssClass.directPrint("p-t-md-auto","padding-top",".%s{%s:auto}");
-responsive += CssClass.directPrint("m-b-md-auto","margin-bottom",".%s{%s:auto}");
-responsive += CssClass.directPrint("p-b-md-auto","padding-bottom",".%s{%s:auto}");
+responsive += CssClass.directPrint("m-r-md-auto", "margin-right", ".%s{%s:auto !important}");
+responsive += CssClass.directPrint("p-r-md-auto", "padding-right", ".%s{%s:auto !important}");
+responsive += CssClass.directPrint("m-l-md-auto", "margin-left", ".%s{%s:auto !important}");
+responsive += CssClass.directPrint("p-l-md-auto", "padding-left", ".%s{%s:auto !important}");
+responsive += CssClass.directPrint("m-t-md-auto", "margin-top", ".%s{%s:auto !important}");
+responsive += CssClass.directPrint("p-t-md-auto", "padding-top", ".%s{%s:auto !important}");
+responsive += CssClass.directPrint("m-b-md-auto", "margin-bottom", ".%s{%s:auto !important}");
+responsive += CssClass.directPrint("p-b-md-auto", "padding-bottom", ".%s{%s:auto !important}");
 
 results += responsiveRapper(meduimScreenResponsive, responsive);
 
 responsive = manyClassesOneAttributeBigScreen.reduce(reducerOneAttribute, "");
-responsive += CssClass.directPrint("m-r-lg-auto","margin-right",".%s{%s:auto}");
-responsive += CssClass.directPrint("p-r-lg-auto","padding-right",".%s{%s:auto}");
-responsive += CssClass.directPrint("m-l-lg-auto","margin-left",".%s{%s:auto}");
-responsive += CssClass.directPrint("p-l-lg-auto","padding-left",".%s{%s:auto}");
-responsive += CssClass.directPrint("m-t-lg-auto","margin-top",".%s{%s:auto}");
-responsive += CssClass.directPrint("p-t-lg-auto","padding-top",".%s{%s:auto}");
-responsive += CssClass.directPrint("m-b-lg-auto","margin-bottom",".%s{%s:auto}");
-responsive += CssClass.directPrint("p-b-lg-auto","padding-bottom",".%s{%s:auto}");
+responsive += CssClass.directPrint("m-r-lg-auto", "margin-right", ".%s{%s:auto !important}");
+responsive += CssClass.directPrint("p-r-lg-auto", "padding-right", ".%s{%s:auto !important}");
+responsive += CssClass.directPrint("m-l-lg-auto", "margin-left", ".%s{%s:auto !important}");
+responsive += CssClass.directPrint("p-l-lg-auto", "padding-left", ".%s{%s:auto !important}");
+responsive += CssClass.directPrint("m-t-lg-auto", "margin-top", ".%s{%s:auto !important}");
+responsive += CssClass.directPrint("p-t-lg-auto", "padding-top", ".%s{%s:auto !important}");
+responsive += CssClass.directPrint("m-b-lg-auto", "margin-bottom", ".%s{%s:auto !important}");
+responsive += CssClass.directPrint("p-b-lg-auto", "padding-bottom", ".%s{%s:auto !important}");
 
 results += responsiveRapper(bigScreenResponsive, responsive);
 
---
+++ readme.md
@@ -0,0 +1,141 @@
+# FastCss
+------
+
+ Are you very tierd to repeat yourself ! 😴😓
+
+ In each project you wrote the same code! 🌌
+
+
+ Are you need to make css code or override the classes for each responsive query ? 🚗🚙🚌🚎
+
+## We Are the Solution ✅✅✅
+
+# Installation
+---
+
+### Just copy and past 💯
+## css
+
+```
+<link rel="stylesheet" href="https://1562868238.rsc.cdn77.org/fastcss.min.css">
+```
+Or  [Download me now !](https://1562868238.rsc.cdn77.org/fastcss.min.css)
+
+## scss
+### [scss Link](https://1562868238.rsc.cdn77.org/fastcss.min.scss)
+
+# Usage
+---
+## key maps
+
+    m: margin;
+
+    p: padding;
+
+    l:left;
+
+    r:right;
+
+    t: top
+
+    b: bottom
+
+    x: left and right
+
+    y: top and button
+
+    fz: font-size
+
+    sm: mobile resolution from 0 to 1024
+
+    md: meduim screen from 1025 to 1599
+
+    lg: large sreen from 1600 to infinity
+
+    auto: auto value.
+### Margin 
+
+static margin from 1px to 100px
+```
+m-1, m-2, m-3 ,...
+```
+static margin in one direction
+```
+m-l-1, m-r-1, m-b-3,m-t-5,...
+```
+
+Static margin in two sides (top and bottom) or (left and right)
+
+```
+p-x-5, p-y-5
+```
+    p-x-5 ->
+         padding-left:5px;
+         padding-right-5px;
+    p-y-5 ->
+         padding-top:5px;
+         padding-bottom-5px;
+### Padding 
+
+static padding from 1px to 100px -> padding: {number}px
+```
+p-auto, p-1, p-2, p-3 ,...
+```
+static padding in one direction
+```
+p-l-1, p-r-1, p-b-3,p-t-5,...
+```
+
+### example
+```
+<div class="p-x-100 p-y-100 m-t-10 m-b-10"></div>
+<div class="p-l-20 p-y-20 p-t-30 p-b-30"></div>
+```
+## Font size
+
+### it can be easily using fz class with the size
+
+```
+fz-1,fz-20, fz-15,fz-16
+```
+    fz-20 -> font-size: 20px;
+
+
+# Media Query
+------
+
+## We have three main resolutions
+
+    mobiles resolution from width 0px to 1024px
+
+    meduim screens from 1025px to 1599px
+
+    Big screens from 1600px and above
+
+### we just add the screen prefix before the number
+
+    [className-prefex-number]
+
+    examples:
+    
+        sm -> fz-sm-10  p-sm-x-10    p-sm-x-20
+        
+        md -> fz-md-20  p-md-x-20    p-md-y-20
+        
+        lg -> fz-lg-30  p-lg-x-20    p-lg-y-20
+    
+# Flex
+
+    f-box -> display:flex
+
+    f-t-c -> align-items: center
+    f-t-start -> align-items: flex-start
+    f-t-end -> align-items: flex-end
+    
+    f-j-c ->        justify-content: center
+    f-j-start ->    justify-content: flex-start
+    f-j-between ->  justify-content: space-between
+    f-j-around ->   justify-content: space-around
+    f-j-evenly ->   justify-content: space-evenly
+    
+## 
